e5297536a41f3546aa9760dbe62d8fe6c211efd7
Arthur Lecert <arthur.lecert@edu.esiee.fr>
2018-04-27 19:21:17 +0200

    Initial Commit

---
+++ app/src/main/java/projet_e3/esiee/com/projet_e3/MainActivity.java
@@ -0,0 +1,13 @@
+package projet_e3.esiee.com.projet_e3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
