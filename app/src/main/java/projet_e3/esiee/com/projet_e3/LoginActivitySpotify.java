67f70eb082ca5c8be358f747e23d0e34aacf20ea
Art <arthur.lecert@edu.esiee.fr>
2018-05-11 10:03:21 +0200

    Code plus clean (Suppression Player inutile donc bugs de connexion réglés) et optimisé

--- app/src/main/java/projet_e3/esiee/com/projet_e3/LoginActivitySpotify.java
+++ app/src/main/java/projet_e3/esiee/com/projet_e3/LoginActivitySpotify.java
@@ -11,12 +11,6 @@ import android.widget.Toast;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
-import com.spotify.sdk.android.player.Config;
-import com.spotify.sdk.android.player.ConnectionStateCallback;
-import com.spotify.sdk.android.player.Error;
-import com.spotify.sdk.android.player.PlayerEvent;
-import com.spotify.sdk.android.player.Spotify;
-import com.spotify.sdk.android.player.SpotifyPlayer;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -25,25 +19,23 @@ import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
 
-public class LoginActivitySpotify extends AppCompatActivity implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
+public class LoginActivitySpotify extends AppCompatActivity {
 
 
     private static final String CLIENT_ID = "1aee09c9f4504604b379f867207fd238";
     private static final String REDIRECT_URI = "smooth-i://logincallback";
-    private String authToken;
-
-    public static SpotifyPlayer mPlayer;
+    private static String authToken;
 
     // Request code that will be used to verify if the result comes from correct activity
     // Can be any integer
-    private static final int REQUEST_CODE = 1468;
+    private static final int REQUEST_CODE = 1337;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI).setShowDialog(true);
-        builder.setScopes(new String[]{/*"user-read-private",*/ "user-top-read", "user-library-read"});
+        builder.setScopes(new String[]{"user-top-read", "user-library-read"});
         AuthenticationRequest request = builder.build();
 
         AuthenticationClient.openLoginActivity(LoginActivitySpotify.this, REQUEST_CODE, request);
@@ -62,25 +54,10 @@ public class LoginActivitySpotify extends AppCompatActivity implements SpotifyPl
                 // Response was successful and contains auth token
                 case TOKEN:
                     // Handle successful response
-                    Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
                     authToken = response.getAccessToken();
-                    Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
-                        @Override
-                        public void onInitialized(SpotifyPlayer spotifyPlayer) {
-                            mPlayer = spotifyPlayer;
-                            mPlayer.addConnectionStateCallback(LoginActivitySpotify.this);
-                            mPlayer.addNotificationCallback(LoginActivitySpotify.this);
-                            mPlayer.login(response.getAccessToken());
-                        }
-
-                        @Override
-                        public void onError(Throwable throwable) {
-                            Log.e("LoginActivitySpotify", "Could not initialize player: " + throwable.getMessage());
-                        }
-                    });
                     Toast.makeText(LoginActivitySpotify.this,"Connexion réussie", Toast.LENGTH_LONG).show();
-                    intent.putExtra("message", "Connecté avec le compte :");
-                    requestData();
+                    requestData("https://api.spotify.com/v1/me/top/tracks");
+                    MainActivity.isLoggedInSpotify = true;
                     startActivity(getBackToMainActivity);
                     break;
 
@@ -102,82 +79,22 @@ public class LoginActivitySpotify extends AppCompatActivity implements SpotifyPl
         }
     }
 
-    @Override
-    protected void onDestroy() {
-        Spotify.destroyPlayer(mPlayer);
-        super.onDestroy();
-    }
-
-    @Override
-    public void onPlaybackEvent(PlayerEvent playerEvent) {
-        Log.d("LoginActivitySpotify", "Playback event received: " + playerEvent.name());
-        switch (playerEvent) {
-            /*case kSpPlaybackNotifyPlay:
-                TextView text = findViewById(R.id.text);
-                String metadata = mPlayer.getMetadata().currentTrack.name;
-                if (metadata.equals(null))
-                    text.setText("NULL");
-                else
-                    text.setText(metadata);*/
-            default:
-                break;
-        }
-    }
-
-    @Override
-    public void onPlaybackError(Error error) {
-        Log.d("LoginActivitySpotify", "Playback error received: " + error.name());
-        switch (error) {
-            // Handle error type as necessary
-            default:
-                break;
-        }
-    }
-
-    @Override
-    public void onLoggedIn() {
-        Log.d("LoginActivitySpotify", "User logged in");
-        // This is the line that plays a song.
-        //mPlayer.playUri(null, "spotify:track:2TpxZ7JUBn3uw46aR7qd6V", 0, 0);
-    }
-
-    @Override
-    public void onLoggedOut() {
-        Log.d("LoginActivitySpotify", "User logged out");
-        //this.onDestroy();
-    }
-
-    @Override
-    public void onLoginFailed(Error var1) {
-        Log.d("LoginActivitySpotify", "Login failed");
-    }
-
-    @Override
-    public void onTemporaryError() {
-        Log.d("LoginActivitySpotify", "Temporary error occurred");
-    }
-
-    @Override
-    public void onConnectionMessage(String message) {
-        Log.d("LoginActivitySpotify", "Received connection message: " + message);
-    }
-
-    public void requestData() {
+    public static void requestData(final String request) {
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
                 try {
                     // Create URL
-                    URL githubEndpoint = new URL("https://api.spotify.com/v1/me/playlists");
+                    URL spotifyEndpoint = new URL(request);
 
                     // Create connection
-                    HttpsURLConnection myConnection = (HttpsURLConnection) githubEndpoint.openConnection();
+                    HttpsURLConnection myConnection = (HttpsURLConnection) spotifyEndpoint.openConnection();
 
                     myConnection.setRequestProperty("Authorization", "Bearer " + authToken);
 
                     if (myConnection.getResponseCode() == 200) {
                         // Success
-                        Log.i("Connection", "REUSSIE !");
+                        Log.i("AsyncTask", "Connection REUSSIE !");
                         InputStream responseBody = myConnection.getInputStream();
                         InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
                         JsonReader jsonReader = new JsonReader(responseBodyReader);
--- app/src/main/java/projet_e3/esiee/com/projet_e3/MainActivity.java
+++ app/src/main/java/projet_e3/esiee/com/projet_e3/MainActivity.java
@@ -4,50 +4,31 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.JsonReader;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.spotify.sdk.android.player.Spotify;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
-
 public class MainActivity extends AppCompatActivity {
+
+    public static boolean isLoggedInSpotify = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textSpotify = findViewById(R.id.textSpotify);
-        //textSpotify.setText(this.getIntent().getStringExtra("message"));
-
         ImageButton logoSpotify = findViewById(R.id.imageButton);
         logoSpotify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(MainActivity.this.isOnline()){
-                    if((LoginActivitySpotify.mPlayer != null) && LoginActivitySpotify.mPlayer.isLoggedIn()){
-                        Toast.makeText(MainActivity.this,"Compte déjà connecté", Toast.LENGTH_LONG).show();
-                    }else{
-                        Intent intent = new Intent(MainActivity.this, LoginActivitySpotify.class);
-                        startActivity(intent);
-                    }
-                }else{
-                    Toast.makeText(MainActivity.this,"Aucune connexion internet détectée", Toast.LENGTH_LONG).show();
-                }
+                if (!MainActivity.isLoggedInSpotify){
+                    Intent intent = new Intent(MainActivity.this, LoginActivitySpotify.class);
+                    startActivity(intent);
+                }else
+                    Toast.makeText(MainActivity.this,"Compte déjà connecté", Toast.LENGTH_LONG).show();
             }
         });
 
@@ -55,60 +36,21 @@ public class MainActivity extends AppCompatActivity {
         buttonDisconnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if((LoginActivitySpotify.mPlayer != null) && LoginActivitySpotify.mPlayer.isLoggedIn()){
-                    LoginActivitySpotify.mPlayer.logout();
+                if(MainActivity.isLoggedInSpotify){
+                    MainActivity.isLoggedInSpotify = false;
                     Toast.makeText(MainActivity.this,"Déconnexion réussie", Toast.LENGTH_LONG).show();
                 }else{
                     Toast.makeText(MainActivity.this,"Aucun compte n'est connecté", Toast.LENGTH_LONG).show();
                 }
-                if((LoginActivitySpotify.mPlayer != null) && !LoginActivitySpotify.mPlayer.isTerminated())
-                    Spotify.destroyPlayer(LoginActivitySpotify.mPlayer);
             }
         });
+    }
 
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    // Create URL
-                    URL githubEndpoint = new URL("https://api.github.com/");
-
-                    // Create connection
-                    HttpsURLConnection myConnection = (HttpsURLConnection) githubEndpoint.openConnection();
-
-                    myConnection.setRequestProperty("User-Agent", "smooth-i-app-v1.0");
-
-                    if (myConnection.getResponseCode() == 200) {
-                        // Success
-                        InputStream responseBody = myConnection.getInputStream();
-                        InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
-                        JsonReader jsonReader = new JsonReader(responseBodyReader);
-
-                        jsonReader.beginObject(); // Start processing the JSON object
-                        while (jsonReader.hasNext()) { // Loop through all keys
-                            String key = jsonReader.nextName(); // Fetch the next key
-                            if (key.equals("organization_url")) { // Check if desired key
-                                // Fetch the value as a String
-                                String value = jsonReader.nextString();
-
-                                // Do something with the value
-                                Log.i("Value", value);
-
-                                break; // Break out of the loop
-                            } else {
-                                jsonReader.skipValue(); // Skip values of other keys
-                            }
-                        }
-                        jsonReader.close();
-                        myConnection.disconnect();
-                    } else {
-                        Log.i("responseCode", "" + myConnection.getResponseCode());
-                    }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(!MainActivity.this.isOnline())
+            Toast.makeText(MainActivity.this,"Aucune connexion internet détectée", Toast.LENGTH_LONG).show();
     }
 
     public boolean isOnline() {
