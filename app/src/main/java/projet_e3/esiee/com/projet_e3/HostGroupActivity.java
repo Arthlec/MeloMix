ed574bdb81b6d166b4bc6e644a6f504d09234172
coren <corentin.valence@edu.esiee.fr>
2018-05-17 16:33:07 +0200

    Nouvelles activités créées et fonctionnels

--- app/src/main/java/projet_e3/esiee/com/projet_e3/HostGroupActivity.java
+++ app/src/main/java/projet_e3/esiee/com/projet_e3/HostGroupActivity.java
@@ -2,6 +2,8 @@ package projet_e3.esiee.com.projet_e3;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,7 +19,7 @@ public class HostGroupActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hostgroup_activity);
 
-        userList = findViewById(R.id.DJ_list);
+        userList = findViewById(R.id.host_list);
         List<String> userRepertoire = new ArrayList<>();
         userRepertoire.add("User 1");
         userRepertoire.add("User 2");
@@ -25,5 +27,13 @@ public class HostGroupActivity extends AppCompatActivity  {
 
         ArrayAdapter<String> userAdapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, userRepertoire);
         userList.setAdapter(userAdapter);
+
+        //ca marche pas ce truc
+        userList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Que faire quand on clique sur un élément de la liste ?
+            }
+        });
     }
 }
