66c6ae4508e694f6fab63947f69a8baf3345fb1a
Git-StevenP <steven.poulain@edu.esiee.fr>
2018-06-01 15:27:56 +0200

    Saved music list implemented in the saved music fragment.

--- app/src/main/java/projet_e3/esiee/com/projet_e3/Fragments/SavedMusicsFragment.java
+++ app/src/main/java/projet_e3/esiee/com/projet_e3/Fragments/SavedMusicsFragment.java
@@ -3,6 +3,8 @@ package projet_e3.esiee.com.projet_e3.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,8 +25,18 @@ public class SavedMusicsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_saved_musics, container, false);
+
+        final RecyclerView rv = view.findViewById(R.id.list);
+
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        rv.setAdapter(new MyAdapter());
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved_musics, container, false);
+        return view;
     }
 
 }
+
+
