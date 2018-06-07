b6c3c8ed6cd0996e23deee53328d5223aaccd63e
Git-StevenP <steven.poulain@edu.esiee.fr>
2018-06-07 15:01:21 +0200

    Saved music deletion with a swipe implemented.

--- app/src/main/java/projet_e3/esiee/com/projet_e3/Fragments/SavedMusicsFragment.java
+++ app/src/main/java/projet_e3/esiee/com/projet_e3/Fragments/SavedMusicsFragment.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +22,7 @@ import projet_e3.esiee.com.projet_e3.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SavedMusicsFragment extends Fragment {
+public class SavedMusicsFragment extends Fragment implements RecyclerItemTouchHelper.RecyclerItemTouchHelperListener {
 
 
     public static SavedMusicsFragment newInstance() {
@@ -29,6 +31,7 @@ public class SavedMusicsFragment extends Fragment {
 
     public static ArrayList<Bitmap> trackCoverList = new ArrayList<>();
     public static ArrayList<String> trackNameList = new ArrayList<>();
+    private MyAdapter myAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,12 +40,27 @@ public class SavedMusicsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_saved_musics, container, false);
 
         RecyclerView rv = view.findViewById(R.id.saved_music_list);
+        myAdapter = new MyAdapter(trackCoverList, trackNameList);
 
         rv.setLayoutManager(new LinearLayoutManager(getContext()));
-        rv.setAdapter(new MyAdapter(trackCoverList, trackNameList));
+        rv.setAdapter(myAdapter);
+
+        ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new RecyclerItemTouchHelper(0, ItemTouchHelper.LEFT, this);
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(rv);
 
         // Inflate the layout for this fragment
         return view;
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
+        if (viewHolder instanceof MyAdapter.MyViewHolder) {
+            // remove the item from recycler view
+            myAdapter.removeItem(viewHolder.getAdapterPosition());
+            // remove the item from the lists
+            trackCoverList.remove(position);
+            trackNameList.remove(position);
+        }
     }
 
 }
