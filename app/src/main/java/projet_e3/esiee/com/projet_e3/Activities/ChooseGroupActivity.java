e15314bbbe7159ff7bd51efc5c7447b8aeefa0be
Dan <dan.seban@edu.esiee.fr>
2018-06-07 16:57:52 +0200

    dialog for 0 guest

--- app/src/main/java/projet_e3/esiee/com/projet_e3/Activities/ChooseGroupActivity.java
+++ app/src/main/java/projet_e3/esiee/com/projet_e3/Activities/ChooseGroupActivity.java
@@ -87,7 +87,7 @@ public class ChooseGroupActivity extends AppCompatActivity implements NumberPick
         public Dialog onCreateDialog(Bundle savedInstanceState) {
 
             final NumberPicker numberPicker = new NumberPicker(getActivity());
-            numberPicker.setMinValue(1);
+            numberPicker.setMinValue(0);
             numberPicker.setMaxValue(10);
 
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -99,11 +99,18 @@ public class ChooseGroupActivity extends AppCompatActivity implements NumberPick
                 public void onClick(DialogInterface dialog, int id) {
                     valueChangeListener.onValueChange(numberPicker,
                             numberPicker.getValue(), numberPicker.getValue());
-                    Intent intent = new Intent(ChooseGroupActivity.this, LoadingHostActivity.class);
-                    intent.putExtra("authToken", getIntent().getStringExtra("authToken"));
-                    intent.putExtra("host", 1);
-                    intent.putExtra("guestNumber", numberPicker.getValue());
-                    startActivity(intent);
+                    if(numberPicker.getValue()==0){
+                        Intent intent = new Intent(ChooseGroupActivity.this, HostActivity.class);
+                        intent.putExtra("authToken", getIntent().getStringExtra("authToken"));
+                        intent.putExtra("host", 1);
+                        startActivity(intent);
+                    } else {
+                        Intent intent = new Intent(ChooseGroupActivity.this, LoadingHostActivity.class);
+                        intent.putExtra("authToken", getIntent().getStringExtra("authToken"));
+                        intent.putExtra("host", 1);
+                        intent.putExtra("guestNumber", numberPicker.getValue());
+                        startActivity(intent);
+                    }
                 }
             });
             builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
