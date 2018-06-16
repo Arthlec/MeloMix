c9ca2a54702ebf1793e7672e01e6e1887eea933d
Dan <dan.seban@edu.esiee.fr>
2018-06-16 18:58:40 +0200

    sauvegarde d'une version avant le merge avec Youtube

--- app/src/main/java/projet_e3/esiee/com/projet_e3/Activities/LoadingGuestActivity.java
+++ app/src/main/java/projet_e3/esiee/com/projet_e3/Activities/LoadingGuestActivity.java
@@ -1,15 +1,29 @@
 package projet_e3.esiee.com.projet_e3.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import projet_e3.esiee.com.projet_e3.R;
 
 public class LoadingGuestActivity extends AppCompatActivity {
+    private HostActivity hostActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_guest);
+        hostActivity = new HostActivity();
+        startActivity();
+    }
+
+    public void startActivity()
+    {
+        Intent intent = new Intent(LoadingGuestActivity.this, hostActivity.getClass());
+        intent.putExtra("authToken", getIntent().getStringExtra("authToken"));
+        intent.putExtra("host",0);
+        findViewById(R.id.loading_panel).setVisibility(View.VISIBLE);
+        startActivity(intent);
     }
 }
